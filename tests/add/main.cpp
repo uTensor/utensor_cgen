a3eb5806248a211f6c706fe9abdb1dac1a91b458
dboyliao <qmalliao@gmail.com>
2017-12-08 02:19:58 +0800

    Daily commit (add tests main.cpp)

--- tests/add/main.cpp
+++ tests/add/main.cpp
@@ -1,17 +1,38 @@
 #include "add_ctx.hpp"
-#include "context.hpp"
 #include "tensorIdxImporter.hpp"
+#include "uTensor_util.hpp"
+#include "test.hpp"
+
+
+class AddTest : public Test {
+    Context ctx;
+    TensorIdxImporter t_import;
+public:
+    void runAll(void) {
+        testStart("simple add test");
+        timer_start();
+        get_test_quant_add_ctx(ctx);
+        S_TENSOR z = ctx.get("z:0");
+        Tensor* ptr_z = z.get();
+        ctx.eval();
+        timer_stop();
+
+        Tensor* ref_z = t_import.float_import("/fs/idx_data/output_z.idx");
+    
+        // compare the results
+        if (ptr_z->getSize() != ref_z->getSize()) {
+            ERR_EXIT("size mismatch");
+        }
+
+        double result = meanPercentErr<float>(ref_z, ptr_z);
+        passed(result < 0.001);
+    }
+};
 
 int main(int argc, char* argv[]) {
 
-    Context ctx;
-    get_test_quant_add_ctx(ctx);
-    Tensor* z = ctx.get("z:0").get();
-
-    TensorIdxImporter t_import;
-    Tensor* ref_z = t_import.float_import("/fs/idx_data/output_z.idx");
-
-    // compare the results
+    AddTest test;
+    test.runAll();
 
     return 0;
 }
