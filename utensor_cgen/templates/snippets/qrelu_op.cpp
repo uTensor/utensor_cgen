3e8ef3733e23563c36dda7485b158ea9aec854bf
dboyliao <qmalliao@gmail.com>
2017-12-08 17:42:57 +0800

    Work around on uTensor issue #64 (https://github.com/neil-tan/uTensor/issues/64)

--- utensor_cgen/templates/snippets/qrelu_op.cpp
+++ utensor_cgen/templates/snippets/qrelu_op.cpp
@@ -1,7 +1,7 @@
 {
     ctx.add(new RamTensor<{{qout_dtype}}>(), "{{outputs[0]}}");
-    ctx.add(new RamTensor<{{out_dtypes[0]}}>(), "{{outputs[1]}}");
-    ctx.add(new RamTensor<{{out_dtypes[1]}}>(), "{{outputs[2]}}");
+    ctx.add(new RamTensor<{{out_dtypes[0]}}>({1}), "{{outputs[1]}}");
+    ctx.add(new RamTensor<{{out_dtypes[1]}}>({1}), "{{outputs[2]}}");
     ctx.push(new ReluOp<{{in_dtype}}, {{out_dtypes[0]}}, {{qout_dtype}}>(), 
              { {% for tname in inputs[:-1]%}"{{tname}}", {% endfor %}"{{inputs[-1]}}" },
              { {% for tname in outputs[:-1]%}"{{tname}}", {% endfor %}"{{outputs[-1]}}" });
--- utensor_cgen/templates/snippets/requant_range_op.cpp
+++ utensor_cgen/templates/snippets/requant_range_op.cpp
@@ -1,7 +1,7 @@
 {
-    ctx.add(new RamTensor<{{out_dtype}}>(), "{{outputs[0]}}");
-    ctx.add(new RamTensor<{{out_dtype}}>(), "{{outputs[1]}}");
+    ctx.add(new RamTensor<{{out_dtype}}>({1}), "{{outputs[0]}}");
+    ctx.add(new RamTensor<{{out_dtype}}>({1}), "{{outputs[1]}}");
     ctx.push(new Requantization_RangeOp(),
-             { {%for tname in inputs[:-1]%}{{tname}}, {% endfor %}{{inputs[-1]}} },
-             { {%for tname in outputs[:-1]%}{{tname}}, {% endfor %}{{outputs[-1]}} });
+             { {%for tname in inputs[:-1]%}"{{tname}}", {% endfor %}"{{inputs[-1]}}" },
+             { {%for tname in outputs[:-1]%}"{{tname}}", {% endfor %}"{{outputs[-1]}}" });
 }
