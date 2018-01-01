f748d4bd30093b9fa97980e9291427a998d8482a
Neil Tan <neil.c.tan@gmail.com>
2018-01-01 23:13:12 +0800

    adding reference counting feature to code generation

--- utensor_cgen/templates/snippets/argmax_op.cpp
+++ utensor_cgen/templates/snippets/argmax_op.cpp
@@ -1,5 +1,5 @@
 {
-    ctx.add(new RamTensor<{{out_dtype}}>(), "{{output}}");
+    ctx.add(new RamTensor<{{out_dtype}}>(), "{{output}}", {{init_count}});
     ctx.push(new ArgMaxOp<{{in_dtype}}, {{out_dtype}}>(), 
              { {% for tname in inputs[:-1]%}"{{tname}}", {%endfor%}"{{inputs[-1]}}" },
              { "{{output}}" });
