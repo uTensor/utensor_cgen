3121ae52c0b2fff493abca892b98615c9c7ec9d4
Neil Tan <neil.c.tan@gmail.com>
2018-01-02 14:53:46 +0800

    missed a file: utensor_cgen/optimizer.py

---
+++ utensor_cgen/optimizer.py
@@ -0,0 +1,15 @@
+from collections import Counter
+
+def get_refc_table(graph_info) -> Counter:
+  refc_table = Counter()
+  for op_name in graph_info:
+    op_info = graph_info[op_name]
+
+    if op_info["op_type"] in ["Const"]:
+      refc_table[op_name] += 1
+    else:
+      for tName, _, _ in op_info["input_tensor"]:
+        refc_table[tName] += 1
+      #add to ref table
+
+  return refc_table
