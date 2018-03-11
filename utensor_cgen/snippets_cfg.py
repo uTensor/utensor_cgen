339d7c131f1095d7e9ce075a7bc2026855f74f28
dboyliao <qmalliao@gmail.com>
2018-03-11 15:38:43 +0800

    Fix syntax error

--- utensor_cgen/snippets_cfg.py
+++ utensor_cgen/snippets_cfg.py
@@ -1,7 +1,7 @@
 # -*- coding:utf8 -*-
 SNIPPET_CONFIG = {
-  "snippets/create_tensor_idx.cpp": set(['"uTensor/loaders/tensorIdxImporter.hpp"', 
-                                         '"uTensor/core/context.hpp"', 
+  "snippets/create_tensor_idx.cpp": set(['"uTensor/loaders/tensorIdxImporter.hpp"',
+                                         '"uTensor/core/context.hpp"',
                                          '"uTensor/core/tensor.hpp"']),
   "snippets/create_tensor_new.cpp": set(['"uTensor/core/context.hpp"', '"uTensor/core/tensor.hpp"']),
   "snippets/add_op.cpp": set(['"uTensor/ops/MathOps.hpp"']),
@@ -15,6 +15,7 @@ SNIPPET_CONFIG = {
   "snippets/reshape_op.cpp": set(['"uTensor/ops/ArrayOps.hpp"']),
   "snippets/requant_op.cpp": set(['"uTensor/ops/MathOps.hpp"']),
   "snippets/requant_range_op.cpp": set(['"uTensor/ops/MathOps.hpp"']),
+  "snippets/conv2d_op.cpp": set(['"uTensor/ops/MatrixOps.hpp"']),
   "snippets/comments.cpp": set([]),
   "snippets/get_ctx.hpp": set(['"uTensor/core/context.hpp"', '"uTensor/core/tensor.hpp"']),
   "containers/main.cpp": set([]),
