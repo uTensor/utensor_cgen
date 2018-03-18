b9652a41e05e269db7fc32ea4a0c46abf9ae72d2
dboyliao <qmalliao@gmail.com>
2018-03-19 00:16:22 +0800

    Add test for conv op

--- tests/conv/main.cpp
+++ tests/conv/main.cpp
@@ -1,12 +1,49 @@
 #include "conv_ctx.hpp"
-#include "tensor.hpp"
-#include "tensorIdxImporter.hpp"
+#include "uTensor/loaders/tensorIdxImporter.hpp"
+#include "uTensor/util/uTensor_util.hpp"
+#include "TESTS/test.hpp"
+#include <mbed.h>
+#include <FATFileSystem.h>
+#include <SDBlockDevice.h>
+
+Serial pc(USBTX, USBRX, 115200);
+SDBlockDevice bd(MBED_CONF_APP_SD_MOSI, MBED_CONF_APP_SD_MISO,
+                 MBED_CONF_APP_SD_CLK, MBED_CONF_APP_SD_CS);
+FATFileSystem fs("fs");
+
+class ConvTest : public Test {
+    Context ctx;
+    TensorIdxImporter t_import;
+public:
+    void runAll(void);
+};
 
 int main(int argc, char* argv[]) {
-    Context ctx;
-    get_test_quant_conv_ctx(ctx);
-    S_TENSOR output = ctx.get("out_conv:0");
-    ctx.eval();
+
+    ON_ERR(bd.init(), "SDBlockDevice init ");
+    ON_ERR(fs.mount(&bd), "Mounting the filesystem on \"/fs\". ");
+
+    ConvTest test;
+    test.runAll();
+    test.printSummary();
+
+    ON_ERR(fs.unmount(), "fs unmount ");
+    ON_ERR(bd.deinit(), "SDBlockDevice de-init ");
 
     return 0;
+}
+
+void ConvTest::runAll(void) {
+    testStart("simple conv test");
+    timer_start();
+    get_test_quant_conv_ctx(ctx);
+    S_TENSOR out = ctx.get("out_conv:0");
+    Tensor* ptr_out = out.get();
+    ctx.eval();
+    timer_stop();
+
+    Tensor* ref_out = t_import.float_import("/fs/idx_data/output_conv.idx");
+    double err = meanPercentErr<float>(ref_out, ptr_out);
+    printf("%f\n", err);
+    passed(err < 0.03);
 }
