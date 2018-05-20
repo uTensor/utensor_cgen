6f1e0021d36a452494fb537bac994c7236299143
dboyliao <qmalliao@gmail.com>
2018-05-20 16:47:24 +0800

    update end-to-end example (mlp)

--- tests/deep_mlp/main.cpp
+++ tests/deep_mlp/main.cpp
@@ -1,4 +1,4 @@
-#include "quant_mnist.hpp"
+#include "simple_mnist.hpp"
 #include "tensorIdxImporter.hpp"
 #include "tensor.hpp"
 #include "mbed.h"
@@ -6,22 +6,22 @@
 #include "C12832.h"
 #include <string>
 
-
 C12832 lcd(SPI_MOSI, SPI_SCK, SPI_MISO, p8, p11);
 EventQueue queue;
 InterruptIn btn(BUTTON1);
 
-void run_mlp(){
+void run_mlp()
+{
     EM_ASM({
         // this writes the content of the canvas (in the simulator) to /fs/tmp.idx
         window.dumpCanvasToTmpFile();
     });
 
     TensorIdxImporter t_import;
-    Tensor* input_x = t_import.float_import("/fs/tmp.idx");
+    Tensor *input_x = t_import.float_import("/fs/tmp.idx");
     Context ctx;
 
-    get_quant_mnist_ctx(ctx, input_x);
+    get_simple_mnist_ctx(ctx, input_x);
     S_TENSOR pred_tensor = ctx.get("y_pred:0");
     ctx.eval();
 
@@ -31,10 +31,11 @@ void run_mlp(){
     lcd.printf("Predicted label: %d", pred_label);
 }
 
-int main(void){
+int main(void)
+{
     init_env();
     printf("Simple MNIST end-to-end uTensor cli example on mbed-simulator\n");
-    
+
     btn.fall(queue.event(&run_mlp));
     queue.dispatch_forever();
 
