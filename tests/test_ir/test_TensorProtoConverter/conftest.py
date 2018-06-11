af6f64b81343ba0ebb6df5fecc4d2ef059296236
dboyliao <qmalliao@gmail.com>
2018-06-12 00:06:12 +0800

    Add test cases

--- tests/test_ir/test_TensorProtoConverter/conftest.py
+++ tests/test_ir/test_TensorProtoConverter/conftest.py
@@ -1,7 +1,19 @@
 import pytest
 import numpy as np
+from tensorflow.core.framework import types_pb2
+from tensorflow.contrib.util import make_tensor_proto
 
 
 @pytest.fixture(scope='session')
 def np_array():
     return np.random.randn(3, 3)
+
+@pytest.fixture(scope='session')
+def tf_qint8_tensor():
+    return make_tensor_proto(127*np.random.rand(3, 3),
+                             types_pb2.DT_QINT8)
+
+@pytest.fixture(scope='session')
+def tf_quint8_tensor():
+    return make_tensor_proto(255*np.random.rand(3, 3),
+                             types_pb2.DT_QUINT8)
--- tests/test_ir/test_TensorProtoConverter/test_converter.py
+++ tests/test_ir/test_TensorProtoConverter/test_converter.py
@@ -1,5 +1,5 @@
 from utensor_cgen.ir.converter import TensorProtoConverter
-
+import numpy as np
 
 def test_np_array(np_array):
     tf_value = TensorProtoConverter.get_tf_value(np_array)
@@ -7,3 +7,18 @@ def test_np_array(np_array):
     generic = TensorProtoConverter.get_generic_value(tf_value)
     assert isinstance(generic, type(np_array))
     assert (generic == np_array).all()
+
+def test_tf_tensor_qint8(tf_qint8_tensor):
+    np_array = TensorProtoConverter.get_generic_value(tf_qint8_tensor)
+    assert np_array.dtype == np.dtype('int8')
+    tf_value = TensorProtoConverter.get_tf_value(np_array)
+    assert tf_value.tensor_content == tf_qint8_tensor.tensor_content
+    assert tf_value.tensor_shape == tf_qint8_tensor.tensor_shape
+
+
+def test_tf_tensor_quint8(tf_quint8_tensor):
+    np_array = TensorProtoConverter.get_generic_value(tf_quint8_tensor)
+    assert np_array.dtype == np.dtype('uint8')
+    tf_value = TensorProtoConverter.get_tf_value(np_array)
+    assert tf_value.tensor_content == tf_quint8_tensor.tensor_content
+    assert tf_value.tensor_shape == tf_quint8_tensor.tensor_shape
