28dd41fdf94374c492522f5ac77eae9e5e042443
dboyliao <qmalliao@gmail.com>
2018-06-24 16:11:24 +0800

    update tests

--- tests/test_transformer/test_pipeline/conftest.py
+++ tests/test_transformer/test_pipeline/conftest.py
@@ -5,9 +5,9 @@ from utensor_cgen.transformer import (RefCntOptimizer, DropoutTransformer,
 
 @pytest.fixture(scope='function', name='methods')
 def pipeline_methods():
-    all_methods = [BatchNormTransformer.KWARGS_NAMESCOPE,
-                   DropoutTransformer.KWARGS_NAMESCOPE,
-                   QuantizeTransformer.KWARGS_NAMESCOPE,
-                   RefCntOptimizer.KWARGS_NAMESCOPE]
+    all_methods = [BatchNormTransformer.METHOD_NAME,
+                   DropoutTransformer.METHOD_NAME,
+                   QuantizeTransformer.METHOD_NAME,
+                   RefCntOptimizer.METHOD_NAME]
     shuffle(all_methods)
     return all_methods
