2dd1a572a726c4731768e631c1f4226044d25be2
Neil Tan <neil.c.tan@gmail.com>
2018-10-07 13:59:02 -0400

    added cli's --save_graph option

--- utensor_cgen/cli.py
+++ utensor_cgen/cli.py
@@ -55,8 +55,13 @@ def cli():
               default="models",
               help="ouptut directory for tensor data idx files",
               show_default=True)
+@click.option("--save_graph",
+              is_flag=True,
+              default=False,
+              help="save the transformed graph",
+              show_default=True)
 def convert_graph(pb_file, output, data_dir, embed_data_dir,
-                  debug_comment, output_nodes, transform_methods, model_dir):
+                  debug_comment, output_nodes, transform_methods, model_dir, save_graph):
   from utensor_cgen.code_generator import CodeGenerator
 
   if pb_file is None:
