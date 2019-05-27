e5c665e6989e88d37320f0156d5d61f553638d91
dboyliao <qmalliao@gmail.com>
2019-05-27 23:57:20 +0800

    Add tests for linear reorder transformer

---
+++ tests/test_transformer/test_linear_reorder/test_transformer.py
@@ -0,0 +1,12 @@
+def test_linear_reorder_1(subj_ugraph_1):
+    from utensor_cgen.transformer.linear_reoder import LinearReorderTransformerV2
+
+    transformer = LinearReorderTransformerV2()
+    new_ugraph = transformer.transform(subj_ugraph_1)
+    for op in new_ugraph['output'].input_nodes:
+        assert op.op_type == 'Relu', 'expecting Relu, get {}'.format(op.op_type)
+        pool_op = op.input_nodes[0]
+        assert pool_op.op_type == 'MaxPool', \
+            'expecting MaxPool as input of Relu, get {}'.format(pool_op.op_type)
+        assert pool_op.op_attr['ksize'].value.ints_value == [1, 3, 3, 1]
+        assert pool_op.op_attr['strides'].value.ints_value == [1, 2, 2, 1]
