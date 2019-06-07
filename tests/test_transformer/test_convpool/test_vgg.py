a0c6ae6d7a5e97732ff035200e1ef042b9a13ed9
dboyliao <qmalliao@gmail.com>
2019-06-08 00:31:10 +0800

    refactor test script a bit

--- tests/test_transformer/test_convpool/test_vgg.py
+++ tests/test_transformer/test_convpool/test_vgg.py
@@ -1,38 +1,23 @@
 from utensor_cgen.transformer.pipeline import TransformerPipeline
 
 
-def test_vgg1(vgg_ugraph):
-    trans = TransformerPipeline([
-        ('linear_reorder_v2', {}),
-        ('quantize', {}),
-        ('conv_pool_v2', {})
-    ])
-    new_ugraph = trans.transform(vgg_ugraph)
-    num_conv = len(vgg_ugraph.get_ops_by_type('Conv2D'))
-    num_convpool = len(new_ugraph.get_ops_by_type('QuantizedFusedConv2DMaxpool'))
-    assert not new_ugraph.get_ops_by_type('Conv2D')
-    assert num_conv == num_convpool
+def factory():
+    def test(vgg_ugraph):
+        trans = TransformerPipeline([
+            ('linear_reorder_v2', {}),
+            ('quantize', {}),
+            ('conv_pool_v2', {})
+        ])
+        new_ugraph = trans.transform(vgg_ugraph)
+        num_conv = len(vgg_ugraph.get_ops_by_type('Conv2D'))
+        num_convpool = len(new_ugraph.get_ops_by_type('QuantizedFusedConv2DMaxpool'))
+        assert not new_ugraph.get_ops_by_type('Conv2D')
+        assert num_conv == num_convpool
+    return test
 
-def test_vgg2(vgg_ugraph):
-    trans = TransformerPipeline([
-        ('linear_reorder_v2', {}),
-        ('quantize', {}),
-        ('conv_pool_v2', {})
-    ])
-    new_ugraph = trans.transform(vgg_ugraph)
-    num_conv = len(vgg_ugraph.get_ops_by_type('Conv2D'))
-    num_convpool = len(new_ugraph.get_ops_by_type('QuantizedFusedConv2DMaxpool'))
-    assert not new_ugraph.get_ops_by_type('Conv2D')
-    assert num_conv == num_convpool
-
-def test_vgg3(vgg_ugraph):
-    trans = TransformerPipeline([
-        ('linear_reorder_v2', {}),
-        ('quantize', {}),
-        ('conv_pool_v2', {})
-    ])
-    new_ugraph = trans.transform(vgg_ugraph)
-    num_conv = len(vgg_ugraph.get_ops_by_type('Conv2D'))
-    num_convpool = len(new_ugraph.get_ops_by_type('QuantizedFusedConv2DMaxpool'))
-    assert not new_ugraph.get_ops_by_type('Conv2D')
-    assert num_conv == num_convpool
+# 5 random tests
+test_vgg1 = factory()
+test_vgg2 = factory()
+test_vgg3 = factory()
+test_vgg4 = factory()
+test_vgg5 = factory()
