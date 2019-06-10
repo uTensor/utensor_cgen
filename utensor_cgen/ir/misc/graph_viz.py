115bfa59e3c2b47ee1fee427d47f61eb17f955ba
dboyliao <qmalliao@gmail.com>
2019-06-11 00:17:29 +0800

    enhance graph viz

--- utensor_cgen/ir/misc/graph_viz.py
+++ utensor_cgen/ir/misc/graph_viz.py
@@ -3,25 +3,58 @@ from graphviz import Digraph
 from utensor_cgen.logger import logger
 
 
-def viz_graph(ugraph, out_fname=None, view=False, cleanup=True):
+def viz_graph(ugraph, out_fname=None, view=False, cleanup=True, colored_nodes=None, suffix=''):
+  if colored_nodes is None:
+    colored_nodes = set()
+  else:
+    colored_nodes = set(colored_nodes)
   dot = Digraph()
   nodes = {}
   i = 0
   for node in ugraph.ops:
-    nodes[node.name] = chr(ord('a') + i)
-    dot.node(nodes[node.name], "%s: %s" % (node.name, node.op_type))
+    nodes[node.name] = '{}_{}'.format(chr(ord('a') + i), suffix)
+    colored = node.name in colored_nodes
+    if colored:
+      dot.node(
+        nodes[node.name],
+        "%s: %s" % (node.name, node.op_type),
+        color='lightskyblue1',
+        style='filled'
+      )
+    else:
+      dot.node(nodes[node.name], "%s: %s" % (node.name, node.op_type))
     i += 1
     for n in node.input_tensors:
       if n.name in nodes:
         continue
-      nodes[n.name] = chr(ord('a') + i)
-      dot.node(nodes[n.name], "%s: Tensor" % n.name)
+      nodes[n.name] = '{}_{}'.format(chr(ord('a') + i), suffix)
+      colored = n.name in colored_nodes
+      if colored:
+        dot.node(
+          nodes[n.name],
+          "%s: Tensor" % n.name,
+          color='olivedrab2',
+          style='filled',
+          shape='box',
+        )
+      else:
+        dot.node(nodes[n.name], "%s: Tensor" % n.name, shape='box')
       i += 1
     for n in node.output_tensors:
       if n.name in nodes:
         continue
-      nodes[n.name] = chr(ord('a') + i)
-      dot.node(nodes[n.name], "%s: Tensor" % n.name)
+      nodes[n.name] = '{}_{}'.format(chr(ord('a') + i), suffix)
+      colored = n.name in colored_nodes
+      if colored:
+        dot.node(
+          nodes[n.name],
+          "%s: Tensor" % n.name,
+          color='olivedrab2',
+          style='filled',
+          shape='box',
+        )
+      else:
+        dot.node(nodes[n.name], "%s: Tensor" % n.name, shape='box')
       i += 1
   for node in ugraph.ops:
     for n in node.input_tensors:
