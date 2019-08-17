d9e5dffe06487814024e1f4012b6da744cac1750
dboyliao <qmalliao@gmail.com>
2019-08-17 23:46:45 +0800

    Basic tests for graph constructor

---
+++ tests/test_graph_constructor/test_ops/test_op_const.py
@@ -0,0 +1,14 @@
+import numpy as np
+
+
+def test_op_const(ugraph, quant_trans):
+    out_tensor, = ugraph.add_op(
+        op_type='Const',
+        name='ones',
+        value=np.ones((3, 3), dtype=np.dtype('float32')),
+        is_output=True
+    )
+    ugraph.finalize()
+
+    assert out_tensor.shape == [3, 3]
+    quant_trans.transform(ugraph)
