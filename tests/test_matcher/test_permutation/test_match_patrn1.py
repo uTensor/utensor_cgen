165ded054c70691d0670bb517feadbf9bf9e7fb9
dboyliao <qmalliao@gmail.com>
2019-11-06 10:56:17 -0600

    update tests

--- tests/test_matcher/test_permutation/test_match_patrn1.py
+++ tests/test_matcher/test_permutation/test_match_patrn1.py
@@ -6,10 +6,19 @@ def test_id_match(patrn_ugraph):
     matches = matcher.match(patrn_ugraph)
     assert matches, 'expecting matches, get {} matches'.format(len(matches))
     match = matches[0]
-    for name, op in match.patrn2subj_op_map.items():
-        assert name == op.name, '{} <--> {}'.format(name, op.name)
-    for name, op in match.subj2patrn_op_map.items():
-        assert name == op.name, '{} <--> {}'.format(name, op.name)
+
+    assert match.patrn2subj_op_map['input0'].name in ['input0', 'input1']
+    assert match.patrn2subj_op_map['input1'].name in ['input0', 'input1']
+    assert match.patrn2subj_op_map['input0'].name != match.patrn2subj_op_map['input1'].name
+    assert match.patrn2subj_op_map['add0'].name == 'add0'
+    assert match.patrn2subj_op_map['output'].name == 'output'
+    
+    assert match.subj2patrn_op_map['input0'].name in ['input0', 'input1']
+    assert match.subj2patrn_op_map['input1'].name in ['input0', 'input1']
+    assert match.subj2patrn_op_map['input0'].name != match.subj2patrn_op_map['input1'].name
+    assert match.subj2patrn_op_map['add0'].name == 'add0'
+    assert match.subj2patrn_op_map['output'].name == 'output'
+
     for tensor in patrn_ugraph.input_tensors:
         assert tensor.name in match.patrn2subj_tensor_map, \
             '{} is missing'.format(tensor.name)
@@ -23,10 +32,11 @@ def test_match_sub1(patrn_ugraph, subject_ugraph1):
     assert matches, 'expecting matches, get {} matches'.format(len(matches))
     match = matches[0]
     assert len(matches) == 2, 'should be exactly two match, get {}'.format(len(matches))
-    assert match.patrn2subj_op_map['input0'].name == 'sub_input0'
-    assert match.patrn2subj_op_map['input1'].name == 'sub_input1'
-    assert match.patrn2subj_op_map['add0'].name == 'sub_add0'
-    assert match.patrn2subj_op_map['output'].name == 'sub_add1'
+    assert match.patrn2subj_op_map['input0'].name in ['sub_input0', 'sub_input1'], match
+    assert match.patrn2subj_op_map['input1'].name in ['sub_input0', 'sub_input1'], match
+    assert match.patrn2subj_op_map['input0'].name != match.patrn2subj_op_map['input1'].name
+    assert match.patrn2subj_op_map['add0'].name == 'sub_add0', match
+    assert match.patrn2subj_op_map['output'].name == 'sub_add1', match
 
 def test_match_sub1_1(patrn_ugraph, subject_ugraph1_1):
     matcher = uTensorGraphMatcher(patrn_ugraph)
