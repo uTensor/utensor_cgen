12b65f2b21a1f537f385a63ba74ce43c5cc435a7
dboyliao <qmalliao@gmail.com>
2019-12-30 23:31:22 +0800

    extension support

--- utensor_cgen/__init__.py
+++ utensor_cgen/__init__.py
@@ -1,7 +1,12 @@
+import sys
+
 import pkg_resources
+
+from ._extensions import _ExtensionsLoader
 
 __version__ = (
   pkg_resources
   .get_distribution('utensor_cgen')
   .version
 )
+sys.modules['utensor_cgen.extensions'] = _ExtensionsLoader()
