e6152755be3c56686f45685dd3d306c28c2743d3
dboyliao <qmalliao@gmail.com>
2020-01-14 23:38:01 +0800

    update tests

--- tests/test_transformer/test_linear_reorder/conftest.py
+++ tests/test_transformer/test_linear_reorder/conftest.py
@@ -11,9 +11,9 @@ def subject_ugraph_1():
         input_1 = tf.placeholder(dtype=tf.float32, shape=[None, 512, 512, 10], name='input_1')
         relu_1 = tf.nn.relu(input_1, name='relu_1')
         max_pool_1 = tf.nn.max_pool(relu_1, ksize=[1, 3, 3, 1], strides=[1, 2, 2, 1], padding='VALID', name='pool_1')
-        input_2 = tf.placeholder(dtype=tf.float32, shape=[None, 512, 512, 3], name='input_2')
-        relu_2 = tf.nn.relu(input_1, name='relu_2')
-        max_pool_2 = tf.nn.max_pool(relu_1, ksize=[1, 3, 3, 1], strides=[1, 2, 2, 1], padding='VALID', name='pool_2')
+        input_2 = tf.placeholder(dtype=tf.float32, shape=[None, 512, 512, 10], name='input_2')
+        relu_2 = tf.nn.relu(input_2, name='relu_2')
+        max_pool_2 = tf.nn.max_pool(relu_2, ksize=[1, 3, 3, 1], strides=[1, 2, 2, 1], padding='VALID', name='pool_2')
         output = tf.add(max_pool_1, max_pool_2, name='output')
-    subj_ugraph = GraphDefParser.parse(graph.as_graph_def(), output_nodes=['output'])
+    subj_ugraph = GraphDefParser.parse(graph.as_graph_def(), output_nodes=[output.op.name])
     return subj_ugraph
