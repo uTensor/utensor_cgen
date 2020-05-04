1d6e3c76150811be011dd90ba526224631b7afbb
dboyliao <qmalliao@gmail.com>
2020-05-04 17:13:00 +0800

    add tests

--- tests/test_frontend/conftest.py
+++ tests/test_frontend/conftest.py
@@ -11,3 +11,12 @@ def tflm_mnist_path():
         'quant_mnist_cnn.tflite'
     )
     return model_path
+
+@pytest.fixture(name='onnx_model_path', scope='session')
+def onnx_model_path():
+    model_path = os.path.join(
+        os.path.dirname(__file__),
+        'model_files',
+        'model.onnx'
+    )
+    return model_path
