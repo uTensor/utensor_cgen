2f84e090cc27a8ead77359ff2959f997eac7b552
dboyliao <qmalliao@gmail.com>
2020-05-05 00:42:51 +0800

    speed up loading time

--- utensor_cgen/__init__.py
+++ utensor_cgen/__init__.py
@@ -5,10 +5,6 @@ import pkg_resources
 # https://github.com/google/or-tools/issues/1830
 # we need to import ortools before tensorflow
 from ortools.sat.python import cp_model as _ # isort:skip
-import tensorflow.compat.v1 as _tf # isort:skip
-
-_tf.disable_v2_behavior() # isort:skip
-_tf.disable_v2_tensorshape() # isort:skip
 
 from utensor_cgen._extensions import _ExtensionsLoader # isort:skip
 
@@ -18,5 +14,3 @@ __version__ = (
   .version
 )
 sys.modules['utensor_cgen.extensions'] = _ExtensionsLoader()
-
-del _tf
