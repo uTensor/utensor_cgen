280343d868667afbb8a75137cedaaaf7acff81d9
dboyliao <qmalliao@gmail.com>
2020-05-09 01:44:39 +0800

    Fix FC filter shape

--- utensor_cgen/legalizer/tflite.py
+++ utensor_cgen/legalizer/tflite.py
@@ -23,6 +23,13 @@ class TFLiteLegalizer(LegalizerBase):
 
   def legalize_ops(self, ugraph):
     self._OpTypeRename.apply(ugraph)
+    for op_info in ugraph.get_ops_by_type('QuantizedFullyConnectedOperator'):
+      filter_tensor = op_info.input_tensors[1]
+      filter_op = filter_tensor.op
+      assert filter_tensor is filter_op.output_tensors[0]
+      np_arr = filter_op.op_attr['value'].value.np_array.reshape(filter_tensor.shape)
+      filter_op.op_attr['value'].value.np_array = np_arr.T
+      filter_tensor.shape = list(np_arr.T.shape)
     return ugraph
   
   def legalize_dtype(self, ugraph):
