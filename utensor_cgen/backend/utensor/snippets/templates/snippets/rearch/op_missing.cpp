8f55c17c2d2cfe92fe7c9e635f323896fea3489b
dboyliao <qmalliao@gmail.com>
2020-05-30 22:19:07 +0800

    update template file

--- utensor_cgen/backend/utensor/snippets/templates/snippets/rearch/op_missing.cpp
+++ utensor_cgen/backend/utensor/snippets/templates/snippets/rearch/op_missing.cpp
@@ -8,13 +8,12 @@
 
     Output Tensors:
     {%for name, var_name in zip(out_tensor_names, out_var_names)%}
-        {{name}}: should be named as {{var_name}}
+        {{name}} should be named as {{var_name}}
         {%if quant_params_map[name]%}
-
-        quantization parameters
-        zero point: {{quant_params_map[name]["zero_point"]["value"]}}, {{quant_params_map[name]["zero_point"]["type_str"]}}
-        scale: {{quant_params_map[name]["scale"]["value"]}}, {{quant_params_map[name]["scale"]["type_str"]}}
-        is per tensor quantization: quant_params_map[name]["is_per_tensor"]
+            quantization parameters:
+            - zero point: {{quant_params_map[name]["zero_point"]["value"]}}, {{quant_params_map[name]["zero_point"]["type_str"]}}
+            - scale: {{quant_params_map[name]["scale"]["value"]}}, {{quant_params_map[name]["scale"]["type_str"]}}
+            - is per tensor quantization: {{quant_params_map[name]["is_per_tensor"]}}
         {%endif%}
     {%endfor%}
     */
