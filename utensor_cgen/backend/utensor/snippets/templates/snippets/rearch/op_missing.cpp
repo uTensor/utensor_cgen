2237dd9059e6e5072a7ca4b375eb5a1b8cbe0b1b
dboyliao <qmalliao@gmail.com>
2020-05-30 22:45:37 +0800

    update template file

--- utensor_cgen/backend/utensor/snippets/templates/snippets/rearch/op_missing.cpp
+++ utensor_cgen/backend/utensor/snippets/templates/snippets/rearch/op_missing.cpp
@@ -3,12 +3,12 @@
 
     Input Tensors:
     {%for tensor in input_tensors%}
-        {{tensor.name}}, of type {{tensor.dtype}}
+        - {{tensor.name}}, of type {{tensor.dtype}}
     {%endfor%}
 
     Output Tensors:
     {%for tensor, var_name in zip(output_tensors, out_var_names)%}
-        {{tensor.name}} is of type {{tensor.dtype}} and should be named as {{var_name}}
+        - {{tensor.name}} is of type {{tensor.dtype}}, shape {{tensor.shape}} and should be named as {{var_name}}
         {%if quant_params_map[tensor.name]%}
             quantization parameters:
             - zero point: {{quant_params_map[tensor.name]["zero_point"]["value"]}}, {{quant_params_map[tensor.name]["zero_point"]["type_str"]}}
@@ -17,3 +17,5 @@
         {%endif%}
     {%endfor%}
     */
+    // Don't forget to comment this out when you fill up the missing op and tensors
+    static_assert(false, "{{op_type}} is not currently supported. Read information above please.");
