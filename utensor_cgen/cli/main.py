84c399985f8e113c4151ffad9487e7f0e3fd5837
dboyliao <qmalliao@gmail.com>
2020-12-02 15:39:51 +0800

    enable plugin loading via extensions, which are packages following specific naming convention

--- utensor_cgen/cli/main.py
+++ utensor_cgen/cli/main.py
@@ -7,16 +7,30 @@ from pathlib import Path
 import click
 
 from utensor_cgen import __version__
+from utensor_cgen.logger import logger
 
 
-def _load_plugin(path):
-  path = Path(path)
-  if not path.exists():
-    raise RuntimeError('{} does not exists'.format(path))
-  sys.path.insert(0, str(path.parent))
-  mod_name = re.sub(r'.py$', '', path.name.split()[0])
-  importlib.import_module(mod_name)
-  sys.path.pop(0)
+def _load_plugin(path_or_name):
+  path = Path(path_or_name)
+  if path.exists():
+    # load as plugin
+    logger.info(
+      f'plugin found at path {path_or_name!r}, loading plugin'
+    )
+    sys.path.insert(0, str(path.parent))
+    mod_name = re.sub(r'.py$', '', path.name.split()[0])
+    importlib.import_module(mod_name)
+    sys.path.pop(0)
+  else:
+    # load as extension
+    # an extension should follow naming convention: utensor_<extension name>
+    logger.info(f"trying to load {path_or_name!r} as extension")
+    try:
+      ext_name = f'utensor_{path_or_name}'
+      importlib.import_module(ext_name)
+    except ImportError as err:
+      raise RuntimeError(f'Fail to load plugin/extension: {ext_name}') from err
+  logger.info(f"{path_or_name!r} loaded")
 
 @click.group(name='utensor-cli')
 @click.help_option('-h', '--help')
