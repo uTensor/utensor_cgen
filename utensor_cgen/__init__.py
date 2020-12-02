3034cb72800c77cd19a0db1d4782bb9d980eba3c
dboyliao <qmalliao@gmail.com>
2020-12-02 15:30:29 +0800

    remove extension loader. Use namespace package instead

--- utensor_cgen/__init__.py
+++ utensor_cgen/__init__.py
@@ -9,11 +9,8 @@ import pkg_resources
 # we need to import ortools before tensorflow
 from ortools.sat.python import cp_model as _
 
-from utensor_cgen._extensions import _ExtensionsLoader
-
 __version__ = (
   pkg_resources
   .get_distribution('utensor_cgen')
   .version
 )
-sys.modules['utensor_cgen.extensions'] = _ExtensionsLoader()
