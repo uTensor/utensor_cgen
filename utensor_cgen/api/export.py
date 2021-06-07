06724be0658ab3e195ec5265d6faee74176f58b7
dboyliao <qmalliao@gmail.com>
2021-06-07 14:30:21 +0800

    cleanup export api code

--- utensor_cgen/api/export.py
+++ utensor_cgen/api/export.py
@@ -15,20 +15,21 @@ def tflm_keras_export(
   representive_dataset,
   model_name=None,
   optimizations=None,
-  config_file='utensor_cli.toml',
-  target='utensor',
-  output_tflite_fname=None
+  supported_ops=None,
+  config_file="utensor_cli.toml",
+  target="utensor",
+  output_tflite_fname=None,
 ):
-  with tempfile.TemporaryDirectory(prefix='utensor_') as tmp_dir:
+  with tempfile.TemporaryDirectory(prefix="utensor_") as tmp_dir:
     dir_path = Path(tmp_dir)
     if isinstance(model_or_path, str):
       converter = tf.lite.TFLiteConverter.from_saved_model(model_or_path)
     elif isinstance(model_or_path, tf.keras.Model):
-      model_path = os.path.join(str(dir_path), 'saved_model')
+      model_path = os.path.join(str(dir_path), "saved_model")
       tf.keras.models.save_model(
         model_or_path,
         model_path,
-        save_format='tf',
+        save_format="tf",
       )
       converter = tf.lite.TFLiteConverter.from_saved_model(model_path)
     else:
@@ -39,29 +40,34 @@ def tflm_keras_export(
       )
     if optimizations is None:
       optimizations = [tf.lite.Optimize.DEFAULT]
+    # https://www.tensorflow.org/lite/guide/ops_select
+    if supported_ops is not None:
+      converter.target_spec.supported_ops = supported_ops
     converter.representative_dataset = representive_dataset
     converter.optimizations = optimizations
     tflm_model_content = converter.convert()
 
-    with (dir_path / 'tflm_model.tflite').open('wb') as fid:
+    with (dir_path / "tflm_model.tflite").open("wb") as fid:
       fid.write(tflm_model_content)
       fid.flush()
-      convert_graph(fid.name, config=config_file, model_name=model_name, target=target)
+      convert_graph(
+        fid.name, config=config_file, model_name=model_name, target=target
+      )
   if output_tflite_fname:
-    with open(output_tflite_fname, 'wb') as fid:
+    with open(output_tflite_fname, "wb") as fid:
       fid.write(tflm_model_content)
+
 
 def pytorch_onnx_export(
   model_or_path,
   representive_dataset,
   model_name=None,
-  optimizations=None,
-  config_file='utensor_cli.toml',
-  target='utensor',
+  config_file="utensor_cli.toml",
+  target="utensor",
+  verbose=False,
   output_onnx_fname=None,
-  verbose=False
-) :
-  with tempfile.TemporaryDirectory(prefix='utensor_') as tmp_dir:
+):
+  with tempfile.TemporaryDirectory(prefix="utensor_") as tmp_dir:
     dir_path = Path(tmp_dir)
     if isinstance(model_or_path, str):
       model = torch.load(model_or_path)
@@ -74,39 +80,37 @@ def pytorch_onnx_export(
         )
       )
 
-    onnx_path = os.path.join(dir_path,f"{model_name}.onnx")
-    
-    torch.onnx.export(model, 
-      representive_dataset, 
-      onnx_path, 
-      verbose=verbose
-    )
+    onnx_path = os.path.join(dir_path, f"{model_name}.onnx")
+    torch.onnx.export(model, representive_dataset, onnx_path, verbose=verbose)
 
     # Print a human readable representation of the graph
     onnx_model = onnx.load(onnx_path)
     onnx.checker.check_model(onnx_model)
-    
+
     if verbose:
       onnx.helper.printable_graph(onnx_model.graph)
 
-    convert_graph(onnx_path,
+    convert_graph(
+      onnx_path,
       config=config_file,
       model_name=model_name,
       target=target,
     )
+    if output_onnx_fname is not None:
+      Path(output_onnx_fname).parent.mkdir(parents=True, exist_ok=True)
+      torch.onnx.export(model, representive_dataset, output_onnx_fname)
+      print(f"{output_onnx_fname} saved")
+
 
 def keras_onnx_export(
   model_or_path,
-  representive_dataset,
   model_name=None,
-  optimizations=None,
-  config_file='utensor_cli.toml',
-  target='utensor',
-  output_onnx_fname=None):
+  config_file="utensor_cli.toml",
+  target="utensor",
+  output_onnx_fname=None,
+):
 
-  with tempfile.TemporaryDirectory(prefix='utensor_') as tmp_dir:
-    dir_path = Path(tmp_dir)
-
+  with tempfile.TemporaryDirectory(prefix="utensor_") as tmp_dir:
     if isinstance(model_or_path, str):
       model = tf.keras.model.load_model(model_or_path)
     elif isinstance(model_or_path, tf.keras.Model):
@@ -119,12 +123,14 @@ def keras_onnx_export(
       )
 
     # Perform Keras to ONNX conversion
-    onnx_model_name = f'{model_name}.onnx'
+    onnx_model_name = os.path.join(tmp_dir, f"{model_name}.onnx")
     onnx_model = keras2onnx.convert_keras(model, model.name)
     onnx.save_model(onnx_model, onnx_model_name)
-    
-    convert_graph(onnx_model_name, 
-      config=config_file, 
-      model_name=model_name, 
-      target=target
+
+    convert_graph(
+      onnx_model_name, config=config_file, model_name=model_name, target=target
     )
+
+    if output_onnx_fname is not None:
+      Path(output_onnx_fname).parent.mkdir(parents=True, exist_ok=True)
+      onnx.save_model(onnx_model, output_onnx_fname)
