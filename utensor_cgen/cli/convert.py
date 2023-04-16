94a76be19b3e3d50f5d3874ac68d14e283c03814
dboyliao <qmalliao@gmail.com>
2023-04-16 23:28:48 +0800

    WIP

--- utensor_cgen/cli/convert.py
+++ utensor_cgen/cli/convert.py
@@ -18,19 +18,21 @@ from .main import cli
   metavar="NODE_NAME,NODE_NAME,...",
   help="list of output nodes"
 )
+@click.option("--inputs-file", metavar="FILE", help="inputs file for the model (required for PyTorch)")
 @click.option('--config', default='utensor_cli.toml', show_default=True, metavar='CONFIG.toml')
 @click.option('--target',
               default='utensor',
               show_default=True,
               help='target framework/platform'
 )
-def convert_graph(model_file, output_nodes, config, target):
+def convert_graph(model_file, output_nodes, config, target, **kwargs):
   from utensor_cgen.api.convert import convert_graph as _convert_graph
 
   _convert_graph(
     model_file=model_file,
     output_nodes=output_nodes,
     config=config,
-    target=target
+    target=target,
+    **kwargs
   )
   return 0
